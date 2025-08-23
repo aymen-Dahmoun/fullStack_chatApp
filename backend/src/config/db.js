168872b0e6d08f0afff81ea39f94ddd6086c7904
Mouhcen <MAC@MacBook-Pro.local>
2025-08-23 15:06:00 -0700

    changed db config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,20 +1,17 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log('db name: ',process.env.DB_NAME)
-console.log('db user: ',process.env.DB_USER)
-console.log('db pass: ',process.env.DB_PASSWORD)
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    logging: true,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export default sequelize;
